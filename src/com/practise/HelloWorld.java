bc76d1e68a500e5dcd6727b7be462c8afc2c0fd0
Ketan Pralhad Naphade <ketan.naphade@Yash.com>
2022-12-10 09:22:21 +0530

    First commit

---
+++ src/com/practise/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.practise;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("First Line of the program");
+		
+		System.out.println("second Line of the program");
+		
+		
+	}
+	
+}
